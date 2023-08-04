abe53ce7a84cdd8873960fa7126db2d4c9ad291c
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-04 07:46:32 +0530

    CHG | Useeffect Hook with counter app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import { Component, useState } from "react";
+import { Component, useEffect, useState } from "react";
 
-// import React from "react";
+import React from "react";
 
-// const App = () => {
-//   const [count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState(0);
 
-//   const increment = () => {
-//     setCount(count + 1);
-//   };
+  useEffect(() => {
+    document.title = `Clicked ${count} times`;
+  });
 
-//   return (
-//     <div>
-//       <h2>Counter App</h2>
-//       <button onClick={increment}>Clicked {count} times</button>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-class App extends Component {
-  state = {
-    count: 0,
+  const increment = () => {
+    setCount(count + 1);
   };
 
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-  componentDidMount() { //works when page renders
-    document.title = `Clicked ${this.state.count} times`;
-  }
-  componentDidUpdate() { //works when page update
-    document.title = `Clicked ${this.state.count} times`;
-  }
-  render() {
-    return (
-      <div>
-        <h2>Counter App</h2>
-        <button onClick={this.increment}>
-          Clicked {this.state.count} times
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <button onClick={increment}>Clicked {count} times</button>
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     count: 0,
+//   };
+
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
+//   componentDidMount() { //works when page renders
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+//   componentDidUpdate() { //works when page update
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h2>Counter App</h2>
+//         <button onClick={this.increment}>
+//           Clicked {this.state.count} times
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
