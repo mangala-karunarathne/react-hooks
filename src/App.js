514c4c5954543c8d5e32b17412dd20e7adfbf186
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-05 08:30:15 +0530

    CHG | Controlling useEffect's behaviour

--- src/App.js
+++ src/App.js
@@ -4,19 +4,40 @@ import React from "react";
 
 const App = () => {
   const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    fetch(url)
       .then((result) => result.json())
+      // .then((data)=>console.log(data))
       .then((data) => setNews(data?.hits))
       .catch((err) => console.log(err));
   };
   useEffect(() => {
     fetchNews();
-  }, []);
+  }, [url]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+    console.log('searchQuery :', searchQuery); // This will log already updated value but not with latest update ( to get update with latest value have to use useEffect as below)
+  }
+  
+  useEffect(() => { // To check the log when update as it's 
+    console.log('searchQuery ( Use effect) :', searchQuery);
+  }, [searchQuery]);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+}
+
   return (
     <div>
       <h2>News</h2>
-      {console.log("news :", news)}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange}/>
+        <button>Search</button>
+      </form>
       {news.map((n, i) => {
         return <p key={i}>{n?.title}</p>;
       })}
