89e20703d64b147e59ac325cc41328f8d1fbf074
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-07 00:29:48 +0530

    BUG | Code organization

--- src/App.js
+++ src/App.js
@@ -14,8 +14,14 @@ const App = () => {
     fetch(url)
       .then((result) => result.json())
       // .then((data)=>console.log(data))
-      .then((data) => {setNews(data?.hits); setLoading(false)})
-      .catch((err) => {console.log(err); setLoading(false)});
+      .then((data) => {
+        setNews(data?.hits);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   };
   useEffect(() => {
     fetchNews();
@@ -35,18 +41,24 @@ const App = () => {
     e.preventDefault();
     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   };
+  const showLoading = () => (loading ? <h2>Loading...</h2> : "");
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button>Search</button>
+    </form>
+  );
+  const showNews = () =>
+    news.map((n, i) => {
+      return <p key={i}>{n?.title}</p>;
+    });
 
   return (
     <div>
       <h2>News</h2>
-      {loading ? <h2>Loading...</h2> : ""}
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={searchQuery} onChange={handleChange} />
-        <button>Search</button>
-      </form>
-      {news.map((n, i) => {
-        return <p key={i}>{n?.title}</p>;
-      })}
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </div>
   );
 };
